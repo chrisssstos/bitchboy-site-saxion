68c48c2033a8310c4b52173787785764645b3176
David Maddin <thedavidmaddin@gmail.com>
2025-05-23 17:02:44 +0200

    Update to visuals

--- project-bitchboy/src/jsx/Footer.jsx
+++ project-bitchboy/src/jsx/Footer.jsx
@@ -3,10 +3,8 @@ import "../css/Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-text">
-        Bitchboy TM - All rights reserved 2025
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <p className="footer-text">Bitchboy TM - All rights reserved 2025</p>
+      <p className="footer-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </footer>
   );
 }
