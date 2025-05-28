8e52bba3deb3120747c36f1f0109eed313a2600a
mykyta <akiplinn@gmail.com>
2025-05-28 15:15:03 +0200

    Finish the footer

--- project-bitchboy/src/components/jsx/Footer.jsx
+++ project-bitchboy/src/components/jsx/Footer.jsx
@@ -1,14 +1,41 @@
 import "../css/Footer.css";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer-text">
-        Bitchboy TM - All rights reserved 2025
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <p className="footer-text">
+                BLOCK SYSTEM © 2023
+            </p>
+            <div className="footer-icons">
+                <a
+                    href="https://www.instagram.com/bitchboy.vj/"
+                >
+                    <img
+                        src={"/images/logoinstagram.svg"}
+                        alt={"Instagram"}
+                    />
+                </a>
+                <a>
+                    <img
+                        src={"/images/logoyoutube.svg"}
+                        alt={"Youtube"}
+                    />
+                </a>
+                <a>
+                    <img
+                        src={"/images/logodiscord.svg"}
+                        alt={"Discord"}
+                    />
+                </a>
+                <a>
+                    <img
+                        src={"/images/logofacebook.svg"}
+                        alt={"Facebook"}
+                    />
+                </a>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
