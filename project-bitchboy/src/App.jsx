86d963e4710dc2149852699dd2908f7950f60222
Bao Anh Nguyen <545104@student.saxion.nl>
2025-06-03 14:26:04 +0200

    Complete merge, both the components appear on the main screen

--- project-bitchboy/src/App.jsx
+++ project-bitchboy/src/App.jsx
@@ -1,16 +1,21 @@
 import VJController from './components/VJController';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Stage, PresentationControls } from '@react-three/drei';
+import Model from './components/3DModel';
 
 function App() {
   return (
-    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
-      <color attach="background" args={["#101010"]} />
-      <PresentationControls speed={1.5} polar={[-0.1, Math.PI / 4]}>
-        <Stage environment="sunset" intensity={0.0005}>
-          <Model scale={0.01} />
-        </Stage>
-      </PresentationControls>
-    </Canvas>
+    <>
+    <div className="AppContainer">
+      <div className="UISection">
+        <VJController />
+      </div>
+      <div className="ModelSection">
+        <Model />
+      </div>
+    </div>
+    </>
   );
 }
 
--- project-bitchboy/src/main.jsx
+++ project-bitchboy/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
+import App from './App.jsx'
 import Model from './components/3DModel.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Model />
+    <App />
+    {/* <Model /> */}
   </StrictMode>,
 )
