83e41a5497f0c9fc0d361189c223288bcaf6d909
David <thedavidmaddin@gmail.com>
2025-06-06 05:28:34 +0200

    images and model animation when scrolling

--- project-bitchboy/src/pages/LandingPage.jsx
+++ project-bitchboy/src/pages/LandingPage.jsx
@@ -1,10 +1,11 @@
 import "./css/LandingPage.css";
-import ModelAnimated from "./../components/jsx/LandingPage/ModelAnimated.jsx";
 import ModelSpin from "./../components/jsx/LandingPage/ModelSpin.jsx";
+import ModelScrollAnimated from "./../components/jsx/LandingPage/ModelScrollAnimated.jsx"; // Add this import
 
 function LandingPage() {
   return (
-    <div style={{ minHeight: "100vh", minWidth: "100vw", background: "#ffffff", position: "relative" }}>
+    <div style={{ minHeight: "400vh", minWidth: "100vw", background: "#ffffff", position: "relative" }}> {/* Increased height for scrolling */}
+      <ModelScrollAnimated />
       <ModelSpin />
       <div className="orange-bg"></div>
       <div className="centered-logo-container">
@@ -17,12 +18,25 @@ function LandingPage() {
         </div>
       </div>
 
-      {/* Subscribe button */}
       <button className="subscribe-button">Sign Up for Updates</button>
-{/*
-      <p className="custom-text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
-      <p className="custom-text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p> */}
 
+      {/* Add some content sections to enable scrolling */}
+      <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <h2 style={{ color: "black", fontSize: "3rem" }}>Scroll Down</h2>
+      </div>
+      <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", marginRight: "500px" }}>
+        <h2 style={{ color: "black", fontSize: "3rem" }}>Keep scrolling...</h2>
+      </div>
+      <div style={{ height: "80vh", display: "flex", alignItems: "center", justifyContent: "right", marginRight: "200px"}}>
+        <h2 style={{ color: "black", fontSize: "3rem" }}>Almost there :D</h2>
+      </div>
+
+      <div className="custom-images">
+        <img src="public\images\background\bbprime.png" alt="Pic 1" className="custom-img img1" />
+        <img src="public\images\background\bb1.png" alt="Pic 2" className="custom-img img2" />
+        <img src="public\images\background\bb2.png" alt="Pic 3" className="custom-img img3" />
+        <img src="public\images\background\vjsetup.png" alt="Pic 4" className="custom-img img4" />
+      </div>
     </div>
   );
 }
